fd44bd294d5efc1e4d55bfccea0512179999fcbc
Deorigosa <Deorigosa@users.noreply.github.com>
2019-08-09 16:09:28 +0300

    Put it into module

--- technologic/lib/technologic/simple_log_formatter.rb
+++ technologic/lib/technologic/simple_log_formatter.rb
@@ -1,28 +1,30 @@
 # frozen_string_literal: true
 
-class SimpleLogFormatter < ::Logger::Formatter
-  FORMAT = "\n[%s] %5s -- %s\n"
+module Technologic
+  class SimpleLogFormatter < ::Logger::Formatter
+    FORMAT = "\n[%s] %5s -- %s\n"
 
-  def initialize
-    @datetime_format = "%H:%M:%S:%3N"
-  end
+    def initialize
+      @datetime_format = "%H:%M:%S:%3N"
+    end
 
-  def call(severity, time, _, msg)
-    FORMAT % [ format_datetime(time), severity, msg2str(msg) ]
-  end
+    def call(severity, time, _, msg)
+      FORMAT % [ format_datetime(time), severity, msg2str(msg) ]
+    end
 
-  private
+    private
 
-  def msg2str(msg)
-    case msg
-    when ::String
-      msg
-    when ::Hash
-      msg.map { |k, v| "\n :#{k} => #{v}" }.join
-    when ::Exception
-      "#{msg.message} (#{msg.class})\n#{(msg.backtrace || []).join("\n")}"
-    else
-      msg.inspect
+    def msg2str(msg)
+      case msg
+      when ::String
+        msg
+      when ::Hash
+        msg.map { |k, v| "\n :#{k} => #{v}" }.join
+      when ::Exception
+        "#{msg.message} (#{msg.class})\n#{(msg.backtrace || []).join("\n")}"
+      else
+        msg.inspect
+      end
     end
   end
 end
