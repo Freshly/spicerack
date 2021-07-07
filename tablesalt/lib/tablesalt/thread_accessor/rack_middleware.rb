079a68cfb8d50dfd5cc7b0de71d60953e34d6217
Allen Rettberg <allen.rettberg@freshly.com>
2021-07-06 20:35:31 -0400

    put rack middleware in Tablesalt module

--- tablesalt/lib/tablesalt/thread_accessor/rack_middleware.rb
+++ tablesalt/lib/tablesalt/thread_accessor/rack_middleware.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-module ThreadAccessor
-  class RackMiddleware
-    def initialize(app)
-      @app = app
-    end
+module Tablesalt
+  module ThreadAccessor
+    class RackMiddleware
+      def initialize(app)
+        @app = app
+      end
 
-    # Clears thread variables after request is finished processing.
-    # Make sure this middleware appears +before+ anything that may set
-    # thread variables using ThreadAccessor
-    def call(req)
-      ThreadAccessor.with_isolated_thread_context(logger: @app.logger) { @app.call(req) }
+      # Clears thread variables after request is finished processing.
+      # Make sure this middleware appears +before+ anything that may set
+      # thread variables using ThreadAccessor
+      def call(req)
+        ThreadAccessor.with_isolated_thread_context(logger: @app.logger) { @app.call(req) }
+      end
     end
   end
 end
